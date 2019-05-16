598df58b4d3b815767242b3de01775aa9813b015
max3d <vadim@unikivi.ee>
2019-05-17 00:57:59 +0300

    flex:1 to fix scrollview issue

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 import React from "react";
-import {View,Text} from "react-native";
+import {View, Text} from "react-native";
 import Header from "./src/components/header.js";
 import AlbumList from "./src/components/album-list.js";
 
 export default class App extends React.Component {
 
     render() {
-        return <View>
+        return <View style={{flex: 1}}>
             <Header headerText={"Hello World!"}/>
             <AlbumList/>
         </View>
